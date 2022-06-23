8caaf878036d8ae3ef8a0c8b62c98d7538defbf4
Chris Adams <39322679+falcon9heavy@users.noreply.github.com>
2022-06-23 00:25:46 -0400

    updaes

--- myDicts.py
+++ myDicts.py
@@ -11,24 +11,41 @@ Key things to keep in mind first...
 lists support access by index, like arrays
 Dictionary support access by keys - or hash tables
 
-"""
-D = {'sue': ['qwerty','yuiop'], 'bill': 12, 'snooka': [1,2,3], 'mD' : { 1: 'jane', 2: 'terry'}}
-print(D['bill'])
+Something to rememember: All built-in immutable objects, like tuples, are hashable while
+mutable containers like lists and dictionaries are not hashable.
+So if you want to use a list of values as a key, you need to make it hashable by
+making it a tuple().
 
-for keys in D:
-    print (f'Values are {D.values()} ')
+"""
+
+
+# I almost feel bad for jamming all of this stuff in a dictionary
+# A convoluted dictionary!
+
+D = {
+    'i can\'t type': ['querty','uyiop'],
+     6: 12,
+     'snuka': [1,2,3],
+     4: { 1: 'jane', 2: 'terry'},
+     tuple(['stop','go']):4, # have to hash the list with tuple function
+     }
+
+print(D.items()) # get KV pairs as tuples
+print(D.keys(), D.values())
+
+# the 'in' statement really only evaluates keys
+print(f'Is the word "snuka" found in this dictionary? {"snuka" in D} ')
 
 D = {'spam' : 2, 'ham' : 1, 'eggs' : 3}
-print(D)
 
-print(D['eggs'], 
+print(
+    D['eggs'],
     len(D),          # how many pair?
     'ham' in D,      # is ham one of the keys
     1 in D,          # is 1 a key? and no it won't tell you if its a value
     list(D.keys()),  # stick the keys in a list
     list(D.values()) # stick the values in a list
     )
-
 D['ham'] = ['eddie', 'mike', 'stone', 'matt']
 print(D)
 
@@ -45,33 +62,30 @@ print('chucky' in elements)
 print(elements['chucky'])
 print(elements.get('billy'))
 
-
+"""
 population = {'Shanghai':17.8, 'Istanbul':13.3, 'Karachi':13.0, 'Mumbai':12.5}
 for key in population:
 	print("the population of {} is {}".format(key,population[key]))
 
-print(
-    list(population.items()),
-    list(population.values()),
-    list(population.keys()),
-    population.get('Izzztanbul', 88) # this works like an true, else return other value=
-    )
+#print(
+#    list(population.items()),
+#    list(population.values()),
+#    list(population.keys()),
+#    population.get('Izzztanbul', 88) # this works like an true, else return other value=
+#    )
 
 D = elements
 D2 = {'toast':4, 'muffin':5}
 D.update(D2)
-print(D)
 D3 = {'toast':99}
 D.update(D3)
-print(D)
-
-print(D.pop('toast')) # pop out the toast for the value bitch!
+#print(D)
 
 L = list(population.values())
-print(L, L.pop()) # or whatever you want...what's left
-print(L.pop(0))
+#print(L, L.pop()) # or whatever you want...what's left
+#print(L.pop(0))
 
-print(D)
+
 
 # Question: go ahead and switch the keys and values in D
 # first, can you force the change? dicts are supposed to be immutable
@@ -81,16 +95,13 @@ print(D)
 
 D['hydrant'] = D['hydro']
 del D['hydro']
-print(D)
 
 # or we can reverse that using pop()
 
 D['hydro'] = D.pop('hydrant')
-print("here is my dict", D) # were gonna reverse this list with zip
 
 D_list_keys = list(D.keys())
 D_list_values = list(D.values())
 
 DD = dict(zip(D_list_values, D_list_keys))
-print(DD)
-
+"""
